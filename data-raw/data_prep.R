841be1c8823d3be7974099a7f464d67bd83ea8f3
AlexTSFox <waldinian@Alexs-MacBook-Pro.local>
2020-03-19 20:53:31 -0400

    added unix time

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -2,6 +2,17 @@ library(devtools)
 library(tidyverse)
 
 use_data_raw()
-UserTicks <- read.csv('../climbs.csv') %>%
-  select(-X.1,-X)
+UserTicks <- read.csv('../climbs.csv')
+
+# remove bad dates
+UserTicks$date[ticks$date == '0000-00-00' |
+             ticks$date == '209-03-18' |
+             ticks$date == '204-12-21' |
+             ticks$date == '0-08-06' |
+             ticks$date == '0-08-07'] <- NaN
+
+# add a unix time column
+UserTicks <- UserTicks %>%
+  mutate(unix_date = as.numeric(as.POSIXct(date)))
+
 use_data(UserTicks, overwrite = TRUE)
