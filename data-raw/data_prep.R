672f8b61d3b65c41f1d32875f8b53cb5a3e33da8
AlexTSFox <waldinian@Alexs-MacBook-Pro.local>
2020-03-19 21:26:36 -0400

    still trying to add unix time

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -5,11 +5,11 @@ use_data_raw()
 UserTicks <- read.csv('../climbs.csv')
 
 # remove bad dates
-UserTicks$date[ticks$date == '0000-00-00' |
-             ticks$date == '209-03-18' |
-             ticks$date == '204-12-21' |
-             ticks$date == '0-08-06' |
-             ticks$date == '0-08-07'] <- NaN
+UserTicks$date[UserTicks$date == '0000-00-00' |
+                 UserTicks$date == '209-03-18' |
+                 UserTicks$date == '204-12-21' |
+                 UserTicks$date == '0-08-06' |
+                 UserTicks$date == '0-08-07'] <- NaN
 
 # add a unix time column
 UserTicks <- UserTicks %>%
